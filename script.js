fa099af348c04ac907cd874634c9213f05ee7fcd
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-22 18:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function updateOutputText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Get the HTML element with the ID "output"
+const outputElement = document.getElementById("output");
+
+// Call the function and update the element when the promise resolves
+updateOutputText().then((text) => {
+  outputElement.textContent = text;
+});
+
+function updateOutput(){
+	return new promise ((resolve)=>{
+		setTimeOut(()=>{
+			resolve("Hello, world!")
+		},1000);
+	})
+}
+const outputElement = document.getElementById("output");
+updateOutput().then((text)=>{
+	outputElement.innerText = text;
+});
+	)
+
